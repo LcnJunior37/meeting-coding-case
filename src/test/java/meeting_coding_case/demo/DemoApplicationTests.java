456f3c753205da23b56f3c5dbbcac1499794c608
Luciano Moreira <lcnjunior37@gmail.com>
2026-02-25 17:47:14 -0300

    initial commit

---
+++ src/test/java/meeting_coding_case/demo/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package meeting_coding_case.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
